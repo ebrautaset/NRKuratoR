647c8b8f1e985ba49b31d16c6c7148a769212772
ebrautaset <eirik.brautaset@nrk.no>
2022-05-05 10:09:00 +0200

    oyvind

--- R/NRKuratoR-package.R
+++ R/NRKuratoR-package.R
@@ -1,5 +1,7 @@
 #' @keywords internal
 "_PACKAGE"
+#' TEST 1
+
 
 ## usethis namespace: start
 ## usethis namespace: end
--- R/create_timeseries.R
+++ R/create_timeseries.R
@@ -6,28 +6,50 @@
 #' 
 #' 
 #' @param email Epost til konto du ønsker å spørre etter dataene med
-#' @param from Date(time) for starttidspunkt
-#' @param to Date(time) for sluttidspunkt
+#' @param fra Date(time) for starttidspunkt
+#' @param til Date(time) for sluttidspunkt
 #' @param period Nivået på periodene du ønsker å aggregere på. Tillater MICROSECOND , MILLISECOND , SECOND , MINUTE , HOUR eller DAY
 #' @param amount Angi størrelsen på intervallene i perioden. Hvis period er satt til SECOND og amount til 20, vil vi lage et datasett med 20 sekunds intervaller
-#' @param contentId Angi contentID for pluggen du ønsker å hente data for. Vi støtter p.t. ikke å legge inn flere contentID'er
+#' @param type Angi hva du ønsker å bruke som identifikator - contentId eller url.
+#' @param ID Angi contentID for pluggen du ønsker å hente data for. ContentID behøver polypoly prefix'er og URL'er trenger å bli enkapsulert med %
 #' @return Et datasett som viser etasje og rom for en gitt artikkel på bestemte tidspunkter
 #' @examples
 #' create_timeseries("eirik.brautaset(at)nrk.no","2022-04-03 00:00:00","2022-04-03 23:59:59",1,"SECONDS","pp:1.15918927")
+#' create_timeseries("eirik.brautaset(at)nrk.no","2022-04-03 00:00:00","2022-04-03 23:59:59",1,"SECONDS",type="url", "%paskenotter%")
 #' 
 #' @export
-create_timeseries = function(email,from,to,period="SECOND",amount=60,contentId){
+create_timeseries = function(email,fra,til,period="SECOND",amount=60,type="contentId",contentId){
   require(bigrquery)
   
-  get_df(paste0(
-  "with secs AS (select * 
-  from UNNEST(GENERATE_TIMESTAMP_ARRAY('",from,"', '",to,"', INTERVAL ",amount," ",period,")) AS secs),
+  
+  if(type == "contentId"){
+    get_df(paste0(
+      "with secs AS (select * 
+  from UNNEST(GENERATE_TIMESTAMP_ARRAY('",fra,"', '",til,"', INTERVAL ",amount," ",period,")) AS secs),
   kurator AS (  SELECT startTime,  endTime,contentId,floorNumber,roomNumber
   FROM `nrk-datahub.consumer_facing_views.kurator_time_periods_detailed` 
-  WHERE contentId = '",contentId,"'  AND publishedDate between '",as.Date(from),"'  AND '",as.Date(to),"')  
+  WHERE contentId = '",ID,"'  AND publishedDate between '",as.Date(from),"'  AND '",as.Date(to),"')  
   select * from secs s left join kurator k on s.secs between k.startTime and k.endTime order by 1")
-
-  ,email)
+      
+      ,email)
+  
+  }
+  if(type == "url"){
+    get_df(paste0(
+      "with secs AS (select * 
+  from UNNEST(GENERATE_TIMESTAMP_ARRAY('",fra,"', '",til,"', INTERVAL ",amount," ",period,")) AS secs),
+  kurator AS (  
+ 
+  SELECT * FROM `nrk-datahub.test_tom.kurator_url_and_placement_periods` WHERE url like ",ID," AND (
+  ",fra," >= startTime AND ",til," <= endTime
+)  )  
+  select * from secs s left join kurator k on s.secs between k.startTime and k.endTime order by 1")
+      ,email)
+  }
+  
+  
+  
+  
 
 }
 
